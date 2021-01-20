876a9f7c377cbda871b54892bf91522be43243bb
Joshua Cho <joshdavecho@protonmail.com>
2021-01-19 21:24:53 -0500

    commit tree 3ff5e9db3664e188de412575442e9017efafc9df

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    num = 10000
+    result_answer = "7"
+    if name_hash == {} 
+        return nil 
+    else
+        name_hash.each do |key, value|
+            while value < num
+                num = value
+                result_answer = key
+            end
+        end
+    end
+    result_answer
 end
